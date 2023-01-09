2619c23412ea6babaf50fadca7735a9a661ca3db
unknown <ines.cherif.tn@gmail.com>
2023-01-09 13:09:25 +0100

    getting photos from API

--- src/components/TinderCards/TinderCards.js
+++ src/components/TinderCards/TinderCards.js
@@ -1,38 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import './TinderCards.css';
-//import Ines from "../../img/ines.jpg";
-//import Khaled from "../../img/khaled.jpeg";
 import TinderCard from 'react-tinder-card';
+import axios from "axios";
 
 function TinderCards() {
   const [people, setPeople] = useState([
-    {
-        name:'ines cherif',
-        // url:<img className='header_logo' src={Ines} alt="Ines" />
-        url:"https://cdn.britannica.com/98/236598-050-9F0C5A8D/Mark-Zuckerberg-2019.jpg"
-    },
-    {
-        name:'khaled cherif',
-        // url:<img className='header_logo' src={Khaled} alt="Kahled" />
-        url:"https://cdn.britannica.com/98/236598-050-9F0C5A8D/Mark-Zuckerberg-2019.jpg"
-    },
-    {
-      name:'ines cherif',
-      // url:<img className='header_logo' src={Ines} alt="Ines" />
-      url:"https://cdn.britannica.com/98/236598-050-9F0C5A8D/Mark-Zuckerberg-2019.jpg"
-  },
+  //   {
+  //       name:'ines cherif',
+  //       url:"https://cdn.britannica.com/98/236598-050-9F0C5A8D/Mark-Zuckerberg-2019.jpg"
+  //   },
+  //   {
+  //       name:'khaled cherif',
+  //       url:"https://cdn.britannica.com/98/236598-050-9F0C5A8D/Mark-Zuckerberg-2019.jpg"
+  //   },
+  //   {
+  //     name:'ines cherif',
+  //     url:"https://cdn.britannica.com/98/236598-050-9F0C5A8D/Mark-Zuckerberg-2019.jpg"
+  // },
   ]);
+
+  const fetchPhotos = () =>{
+     fetch('https://jsonplaceholder.typicode.com/photos').then(response =>response.json()).then((json) => setPeople(json));
+  }
+  useEffect(() => {
+    fetchPhotos();
+  }, []);
+
+
   return (
     <div>
-      <h1>tinder cards</h1>
+      {/* <h1>tinder cards</h1> */}
       <div className='tinderCards_cardContainer'>
       {
         people.map((person) =>(
-        <TinderCard className="swipe" key={person.name} preventSwipe={["up", "down"]}> 
+        <TinderCard className="swipe" key={person.id} preventSwipe={["up", "down"]}> 
             <div 
             style={{backgroundImage : `url(${person.url})`}}
             className='card'>
-                <h3>{person.name}</h3>
+                <h3>{person.title}</h3>
             </div>
         </TinderCard>
         ))}
